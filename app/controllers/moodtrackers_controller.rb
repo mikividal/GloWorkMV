66c1263136a7c6c7777c4de30cdc4e92935cdcc9
Maria Micaela Vidal <131964698+mikividal@users.noreply.github.com>
2025-05-29 18:11:38 +0100

    moods loking good

--- app/controllers/moodtrackers_controller.rb
+++ app/controllers/moodtrackers_controller.rb
@@ -14,6 +14,12 @@ class MoodtrackersController < ApplicationController
     end
   end
 
+  def show
+    @moodtrackers = Moodtracker.all
+    @percentage = user_percentage
+    @emojis = emoji_percentage
+  end
+
   def edit
     @moodtracker = Moodtracker.find(params[:id])
   end
@@ -27,8 +33,47 @@ class MoodtrackersController < ApplicationController
     end
   end
 
+  def user_mood
+    @moodtrackers = Moodtracker.all
+    my_moods = 0
+    user_moods = @moodtrackers.where(user_id: current_user.id)
+    user_moods.each do |mood|
+      my_moods += mood.mood
+    end
+    average_mood = my_moods / user_moods.size
+    raise
+    return average_mood
+  end
+
+  def user_percentage
+    @moodtrackers = Moodtracker.all
+    sad = 0
+    neutral = 0
+    happy = 0
+    user_moods = @moodtrackers.where(user_id: current_user.id)
+    user_moods.each do |mood|
+      case mood.mood
+      when 1 then sad += 1
+      when 2 then neutral += 1
+      when 3 then happy += 1
+      end
+    end
+    total = happy + neutral + sad
+    @p_happy = (happy.to_f / total) * 100
+    @p_neutral = (neutral.to_f / total) * 100
+    @p_sad = (sad.to_f / total) * 100
+    { happy: @p_happy.round(2), neutral: @p_neutral.round(2), sad: @p_sad.round(2) }
+  end
+
+  def emoji_percentage
+    [[@p_happy, "😀"], [@p_neutral, "😐"], [@p_sad, "☹️"]].sort_by { |a| a[0] }.reverse
+  end
+
   private
+
   def mood_params
     params.require(:moodtracker).permit(:mood, :comment)
   end
+
+
 end
