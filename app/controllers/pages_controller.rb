ab1d06f138911d6d29ab0a032f444ebce8ffdc26
Maria Micaela Vidal <131964698+mikividal@users.noreply.github.com>
2025-06-04 14:08:29 +0100

    all ok now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,34 +5,12 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    @moodtrackers = Moodtracker.all
-    @percentage = user_percentage
+    @range = params[:range] || "7days"
+    @moodtrackers = filtered_moodtrackers(@range)
+    @percentage = user_percentage(@moodtrackers, current_user)
     @suggestions = Suggestion.all
     @suggestions = Suggestion.order(:created_at)
     @events = Event.all
-  end
-
-  def user_percentage
-    sad = 0
-    neutral = 0
-    happy = 0
-    user_moods = @moodtrackers.where(user_id: current_user.id)
-    user_moods.each do |mood|
-      case mood.mood
-      when 1 then sad += 1
-      when 2 then neutral += 1
-      when 3 then happy += 1
-      end
-    end
-    total = happy + neutral + sad
-    @p_happy = (happy.to_f / total) * 100
-    @p_neutral = (neutral.to_f / total) * 100
-    @p_sad = (sad.to_f / total) * 100
-    { happy: @p_happy.round(2), neutral: @p_neutral.round(2), sad: @p_sad.round(2), emojis: emoji_percentage(@p_happy, @p_sad, @p_neutral) }
-  end
-
-  def emoji_percentage(happy, sad, neutral)
-    [[happy, "😀"], [neutral, "😐"], [sad, "☹️"]].sort_by { |a| a[0] }.reverse
   end
 
   private
