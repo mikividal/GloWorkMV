febad95c844f3a6450a3cd16029c543684069c90
Agnes Vincze <agnes-vincze@hotmail.com>
2025-06-06 13:08:22 +0100

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -138,7 +138,7 @@ user2 = User.create!(
   admin: false,
   job_position: "Employee",
   team: "Sales",
-  location: Faker::Address.country,
+  location: "Australia",
   password: "GloWork2025"
 )
 
